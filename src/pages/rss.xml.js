6741e046b40c0345d73a5b48503cb9b8929dc7ea
clintt-09 <aisosaclinton09@gmail.com>
2025-06-13 16:26:53 +0100

    tags page, static routes and rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, {pagesGlobToRssItems} from "@astrojs/rss"
+
+export async function GET (context){
+    return rss({
+        title: "a1s0sa astro learning | blog",
+        description: "my journey learning Astro",
+        site: context.site,
+        items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+        customData: `<language>en-us</language>`
+    })
+}
